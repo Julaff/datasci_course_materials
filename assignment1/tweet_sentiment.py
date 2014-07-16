9b2403dba15e99b648d423b099e540af468fa93b
Julien <julien.laffargue@gmail.com>
2014-07-17 00:06:04 +0200

    First two parts of Assignment 1

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,7 +1,5 @@
+import json
 import sys
-
-def hw():
-    print 'Hello, world!'
 
 def lines(fp):
     print str(len(fp.readlines()))
@@ -9,9 +7,24 @@ def lines(fp):
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    scores = {} # initialize an empty dictionary
+    for line in sent_file:
+      term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+      scores[term] = int(score)  # Convert the score to an integer.
+    for line in tweet_file:
+      converted = json.loads(line)
+      tweet = converted.get('text',"").encode('utf-8')
+      s = 0
+      for i in range(len(tweet.split())):
+        word = tweet.split()[i]
+#        print word
+        if word in scores:
+          s = s + scores[word]
+      print s
 
+#    print scores.items() # Print every (term, score) pair in the dictionary
+#    print(len(scores))
+#    lines(tweet_file)
+#    print scores.viewkeys()
 if __name__ == '__main__':
     main()
