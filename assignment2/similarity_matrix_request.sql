b15a469b36a6b89b6d8fff1571f4cc9b594ebec1
Julien <julien.laffargue@gmail.com>
2014-07-21 00:03:29 +0200

    question 3h, got lucky

--- assignment2/similarity_matrix_request.sql
+++ assignment2/similarity_matrix_request.sql
@@ -1,3 +1,6 @@
-select sum(a.count*b.count) from Frequency a join Frequency b
+select sum(a.count*b.count)
+from Frequency a
+join Frequency b
+on a.term = b.term
 where a.docid = "10080_txt_crude" and b.docid = "17035_txt_earn"
-group by a.term;
+group by a.docid, b.docid;
