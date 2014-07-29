8fdc1495ded52d57ce565913a33991f1e9a5f7d2
Julien <julien.laffargue@gmail.com>
2014-07-30 00:41:55 +0200

    Finished mapping Problem 6

--- assignment3/multiply.py
+++ assignment3/multiply.py
@@ -11,17 +11,31 @@ mr = MapReduce.MapReduce()
 # Do not modify above this line
 
 def mapper(record):
-    # key: matrix_id
-    # value: value
-    matrix_id = record[0]
-    value = record[3]
-#    trimmed_nucleotides = nucleotides[0:len(nucleotides)-10]
-    mr.emit_intermediate(matrix_id, value)
+    # key: (i,j) then (j,k)
+    # value: A[i,j] then B[j,k]
+#    print record[3]
+    tablename = record[0]
+    if tablename == 'a':
+      for k in range(5):
+        key = (record[1],k)
+        value = (record[2],record[3])
+        mr.emit_intermediate(key, value)
+
+    elif tablename == 'b':
+      for i in range(5):
+        key = (i,record[2])
+        value = (record[1],record[3])
+        mr.emit_intermediate(key, value)
 
 def reducer(key, list_of_values):
-    # key: trimmed_nucleotides
-    # value: sequence_id
-      mr.emit(key)
+#    print 0
+    # key: (i,k)
+    # value: Sum_j(A[i,j]*B[j,k])
+    l = []
+    for c in range(len(list_of_values)):
+      l.append(list_of_values[c][0])
+    print l
+#      mr.emit((key,list_of_values))
 
 # Do not modify below this line
 # =============================
